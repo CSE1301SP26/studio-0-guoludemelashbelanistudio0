956ae79c2e339cde8a53e20ead0a48dd3ea6ba9d
bot-allu <allu@wustl.edu>
2026-01-14 13:32:12 -0600

    Studio 0 Finished

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,5 +4,7 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println(" theasdl;fk   l;k j workshop! ");
+		System.out.println(" theasdl;fk   l;k j workshop! ");
 	}   
 }
+///h iotohsdihto 
